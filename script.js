d28953827895c077520a8a53f1a00c1ac977300a
Albakhsh Sheikh <98633969+AlbakhshSheikh@users.noreply.github.com>
2025-04-08 21:14:42 +0530

    created a submission

--- script.js
+++
@@ -1,6 +0,0 @@
-//your JS code here. If required.
-// Simple alert when page loads
-window.onload = function() {
-    console.log("Welcome to Santorini, Greece!");
-    // Could add more interactive features here if desired
-};
